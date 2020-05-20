e00bbb98cd3cee3297db1a9d317c305d5eac24e0
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-20 09:55:17 -0700

    mm: Don't stop kswapd on a per-node basis when there are no waiters
    The page allocator wakes all kswapds in an allocation context's allowed
    nodemask in the slow path, so it doesn't make sense to have the kswapd-
    waiter count per each NUMA node. Instead, it should be a global counter
    to stop all kswapds when there are no failed allocation requests.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4607,7 +4607,6 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	unsigned long jiffies_s = jiffies;
 	u64 utime, stime_s, stime_e, stime_d;
 	task_cputime(current, &utime, &stime_s);
-	pg_data_t *pgdat = ac->preferred_zoneref->zone->zone_pgdat;
 	bool woke_kswapd = false;
 
 	/*
@@ -4644,15 +4643,9 @@ restart:
 	if (!ac->preferred_zoneref->zone)
 		goto nopage;
 
-<<<<<<< HEAD
 	if (alloc_flags & ALLOC_KSWAPD) {
 		if (!woke_kswapd) {
 			atomic_long_inc(&kswapd_waiters);
-=======
-	if (gfp_mask & __GFP_KSWAPD_RECLAIM) {
-		if (!woke_kswapd) {
-			atomic_inc(&pgdat->kswapd_waiters);
->>>>>>> e3d47985157b (mm: Stop kswapd early when nothing's waiting for it to free pages)
 			woke_kswapd = true;
 		}
 		wake_all_kswapds(order, gfp_mask, ac);
@@ -4873,7 +4866,6 @@ nopage:
 	}
 fail:
 got_pg:
-<<<<<<< HEAD
 	task_cputime(current, &utime, &stime_e);
 	stime_d = stime_e - stime_s;
 	if (stime_d / NSEC_PER_MSEC > 256) {
@@ -4899,10 +4891,6 @@ got_pg:
 	}
 	if (woke_kswapd)
 		atomic_long_dec(&kswapd_waiters);
-=======
-	if (woke_kswapd)
-		atomic_dec(&pgdat->kswapd_waiters);
->>>>>>> e3d47985157b (mm: Stop kswapd early when nothing's waiting for it to free pages)
 	if (!page)
 		warn_alloc(gfp_mask, ac->nodemask,
 				"page allocation failure: order:%u", order);
@@ -6892,7 +6880,6 @@ static void __meminit pgdat_init_internals(struct pglist_data *pgdat)
 	pgdat_page_ext_init(pgdat);
 	spin_lock_init(&pgdat->lru_lock);
 	lruvec_init(node_lruvec(pgdat));
-	pgdat->kswapd_waiters = (atomic_t)ATOMIC_INIT(0);
 }
 
 static void __meminit zone_init_internals(struct zone *zone, enum zone_type idx, int nid,
