ab7e6d42724c9226453760fc374858546b71e750
skye <sonicgames772@gmail.com>
2025-05-25 19:07:57 -0300

    root: funni scripts part 2
    now notkrenol build for r8q(s20 fe) x1q(s20 5g) y2q(s20 5g+) z3s(s20 ultra)

---
+++ bomb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+make O=/home/skye/bomb/out clean
+echo "***************r8q BUILD STARTED***************"
+echo "***************r8q BUILD STARTED***************"
+echo "***************r8q BUILD STARTED***************"
+"./r8q.sh"
+echo "***************r8q BUILD FINISHED***************"
+echo "***************r8q BUILD FINISHED***************"
+echo "***************r8q BUILD FINISHED***************"
+make O=/home/skye/bomb/out clean
+echo "***************x1q BUILD STARTED***************"
+echo "***************x1q BUILD STARTED***************"
+echo "***************x1q BUILD STARTED***************"
+"./x1q.sh"
+echo "***************x1q BUILD FINISHED***************"
+echo "***************x1q BUILD FINISHED***************"
+echo "***************x1q BUILD FINISHED***************"
+make O=/home/skye/bomb/out clean
+echo "***************y2q BUILD STARTED***************"
+echo "***************y2q BUILD STARTED***************"
+echo "***************y2q BUILD STARTED***************"
+"./y2q.sh"
+echo "***************y2q BUILD FINISHED***************"
+echo "***************y2q BUILD FINISHED***************"
+echo "***************y2q BUILD FINISHED***************"
+make O=/home/skye/bomb/out clean
+echo "***************z3q BUILD STARTED***************"
+echo "***************z3q BUILD STARTED***************"
+echo "***************z3q BUILD STARTED***************"
+"./z3q.sh"
+echo "***************z3q BUILD FINISHED***************"
+echo "***************z3q BUILD FINISHED***************"
+echo "***************z3q BUILD FINISHED***************"
+cp /home/skye/bomb/AnyKernel3/r8q/*.zip /home/skye/bomb/AnyKernel3/zip
+cp /home/skye/bomb/AnyKernel3/x1q/*.zip /home/skye/bomb/AnyKernel3/zip
+cp /home/skye/bomb/AnyKernel3/y2q/*.zip /home/skye/bomb/AnyKernel3/zip
+cp /home/skye/bomb/AnyKernel3/z3q/*.zip /home/skye/bomb/AnyKernel3/zip
