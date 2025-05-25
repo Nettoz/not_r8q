a59f66acc26efb901c71d029e10c46e3d7c470b1
skye <sonicgames772@gmail.com>
2025-05-25 17:04:05 -0300

    bob

--- x1q.sh
+++ x1q.sh
@@ -19,9 +19,9 @@ make O=/home/skye/bomb/out $BUILD_ENV vendor/kona-not_defconfig vendor/samsung/x
 echo "*****************************************"
 echo "*****************************************"
 
-make -j12 O=/home/skye/bomb/out $BUILD_ENV dtbs
-DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
-cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/x1q/dtb
+#make -j12 O=/home/skye/bomb/out $BUILD_ENV dtbs
+#DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
+#cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/x1q/dtb
 
 #make -j12 O=/home/skye/bomb/out $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
 DTBO_OUT="/home/skye/bomb/out/arch/arm64/boot"
